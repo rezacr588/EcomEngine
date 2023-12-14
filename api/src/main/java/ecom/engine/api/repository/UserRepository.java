2df1348d2af07ec013d85b56b7ec7447a1d50f8a
rzeraat.tur <rzeraat.tur@gmail.com>
2023-12-14 21:00:15 +0300

    add repository

---
+++ api/src/main/java/ecom/engine/api/repository/UserRepository.java
@@ -0,0 +1,41 @@
+package ecom.engine.api.repository;
+
+import ecom.engine.api.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    List<User> findByLastName(String lastName);
+
+    List<User> findByActiveTrue();
+
+    Page<User> findAll(Pageable pageable);
+
+    @Query("SELECT u FROM User u WHERE u.email LIKE %:email%")
+    List<User> searchByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u SET u.active = :active WHERE u.id = :id")
+    void setActiveForUser(boolean active, Long id);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    long countByActiveTrue();
+}
