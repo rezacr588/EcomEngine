1985ec4795b4ff0a878a1a4bcd3aa700624c3aed
rzeraat.tur <rzeraat.tur@gmail.com>
2023-12-17 02:52:54 +0300

    add product entity

---
+++ api/src/main/java/ecom/engine/api/model/Product.java
@@ -0,0 +1,90 @@
+package ecom.engine.api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private double price;
+
+    @Column(length = 1000) // Assuming a long description
+    private String description;
+
+    private String category;
+
+    // If you're planning to store image URLs
+    private String imageUrl;
+
+    // Constructors
+    public Product() {
+        // Default constructor
+    }
+
+    public Product(String name, double price, String description, String category, String imageUrl) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.imageUrl = imageUrl;
+    }
+
+    // Standard getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    // Additional methods and business logic as needed
+}
