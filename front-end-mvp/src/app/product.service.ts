88ce746ce71d556c7afd94f8ab898f8285b39ad1
rzeraat.tur <rzeraat.tur@gmail.com>
2024-01-18 05:39:27 +0300

    Refactor product service and products component

--- front-end-mvp/src/app/product.service.ts
+++ front-end-mvp/src/app/product.service.ts
@@ -6,19 +6,26 @@ import { Product } from './models/product.model';
   providedIn: 'root',
 })
 export class ProductService {
+  apiUrl = 'http://localhost:8080/api/products';
   constructor(private http: HttpClient) {}
 
   getFeaturedProducts() {
-    return this.http.get<Product[]>(
-      `http://localhost:8080/api/products/featured`
-    );
+    return this.http.get<Product[]>(`${this.apiUrl}/featured`);
   }
 
   addProduct(product: Product) {
-    return this.http.post(`http://localhost:8080/api/products`, product);
+    return this.http.post<Product>(`${this.apiUrl}`, product);
+  }
+
+  getLatestProducts() {
+    return this.http.get<Product[]>(`${this.apiUrl}/latest`);
   }
 
   getAllProducts() {
-    return this.http.get<Product[]>(`http://localhost:8080/api/products`);
+    return this.http.get<Product[]>(`${this.apiUrl}`);
+  }
+
+  removeProduct(id: number) {
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 }
--- front-end-mvp/src/app/products/products.component.ts
+++ front-end-mvp/src/app/products/products.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  featuredProducts: Product[] = []; // Add your featured products data here
+  currentSlideIndex: number = 0;
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.getFeaturedProducts();
+  }
+
+  getFeaturedProducts() {
+    this.productService.getFeaturedProducts().subscribe((products) => {
+      this.featuredProducts = products;
+      this.currentSlide(1); // Start with the first slide
+    });
+  }
+
+  nextSlide() {
+    this.currentSlideIndex =
+      (this.currentSlideIndex + 1) % this.featuredProducts.length;
+    this.currentSlide(this.currentSlideIndex + 1);
+  }
+
+  previousSlide() {
+    this.currentSlideIndex =
+      (this.currentSlideIndex - 1 + this.featuredProducts.length) %
+      this.featuredProducts.length;
+    this.currentSlide(this.currentSlideIndex + 1);
+  }
+
+  currentSlide(index: number) {
+    this.currentSlideIndex = index - 1;
+    // Add logic to change slide based on index
+  }
 }
