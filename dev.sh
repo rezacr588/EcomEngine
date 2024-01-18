f012fb2d9a3c0bcc47fe592a4b657c73959d7814
rzeraat.tur <rzeraat.tur@gmail.com>
2024-01-19 00:05:52 +0300

    Add script to start front-end and API servers

---
+++ dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Navigate to the front-end directory and start the front-end server in the background
+cd front-end-mvp
+yarn start &
+
+# Navigate to the API directory and start the API server
+cd ../api
+mvn spring-boot:run
