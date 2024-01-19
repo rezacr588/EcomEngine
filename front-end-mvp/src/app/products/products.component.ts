311e50fd24626c1e193cec11946cfd14b4a05f46
rzeraat.tur <rzeraat.tur@gmail.com>
2024-01-19 03:48:59 +0300

    Refactor slider styles and HTML structure

--- front-end-mvp/src/app/products/products.component.ts
+++ front-end-mvp/src/app/products/products.component.ts
@@ -8,7 +8,7 @@ import { ProductService } from '../product.service';
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-  featuredProducts: Product[] = []; // Add your featured products data here
+  featuredProducts: Product[] = [];
   currentSlideIndex: number = 0;
 
   constructor(private productService: ProductService) {}
@@ -20,25 +20,17 @@ export class ProductsComponent implements OnInit {
   getFeaturedProducts() {
     this.productService.getFeaturedProducts().subscribe((products) => {
       this.featuredProducts = products;
-      this.currentSlide(1); // Start with the first slide
     });
   }
 
   nextSlide() {
     this.currentSlideIndex =
       (this.currentSlideIndex + 1) % this.featuredProducts.length;
-    this.currentSlide(this.currentSlideIndex + 1);
   }
 
   previousSlide() {
     this.currentSlideIndex =
       (this.currentSlideIndex - 1 + this.featuredProducts.length) %
       this.featuredProducts.length;
-    this.currentSlide(this.currentSlideIndex + 1);
-  }
-
-  currentSlide(index: number) {
-    this.currentSlideIndex = index - 1;
-    // Add logic to change slide based on index
   }
 }
