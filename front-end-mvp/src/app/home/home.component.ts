e5034fb70d185d9e6310a0aea748683231e5374e
rzeraat.tur <rzeraat.tur@gmail.com>
2024-01-19 04:55:59 +0300

    Update home component slider styles and functionality

--- front-end-mvp/src/app/home/home.component.ts
+++ front-end-mvp/src/app/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  featuredProducts: Product[] = [];
+  currentSlideIndex: number = 0;
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.getFeaturedProducts();
+  }
+
+  getFeaturedProducts() {
+    this.productService.getFeaturedProducts().subscribe((products) => {
+      this.featuredProducts = products;
+    });
+  }
+
+  nextSlide() {
+    this.currentSlideIndex =
+      (this.currentSlideIndex + 1) % this.featuredProducts.length;
+  }
+
+  previousSlide() {
+    this.currentSlideIndex =
+      (this.currentSlideIndex - 1 + this.featuredProducts.length) %
+      this.featuredProducts.length;
+  }
 }
