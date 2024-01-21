30bcc4b377113a5b2524ea56c331747e39beab96
rzeraat.tur <rzeraat.tur@gmail.com>
2024-01-21 21:54:33 +0300

    Remove featured products functionality

--- front-end-mvp/src/app/products/products.component.ts
+++ front-end-mvp/src/app/products/products.component.ts
@@ -8,37 +8,18 @@ import { ProductService } from '../product.service';
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-  featuredProducts: Product[] = [];
   latestProducts: Product[] = [];
   currentSlideIndex: number = 0;
 
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
-    this.getFeaturedProducts();
     this.getLatestProducts();
-  }
-
-  getFeaturedProducts() {
-    this.productService.getFeaturedProducts().subscribe((products) => {
-      this.featuredProducts = products;
-    });
   }
 
   getLatestProducts() {
     this.productService.getLatestProducts().subscribe((products) => {
       this.latestProducts = products;
     });
-  }
-
-  nextSlide() {
-    this.currentSlideIndex =
-      (this.currentSlideIndex + 1) % this.featuredProducts.length;
-  }
-
-  previousSlide() {
-    this.currentSlideIndex =
-      (this.currentSlideIndex - 1 + this.featuredProducts.length) %
-      this.featuredProducts.length;
   }
 }
