a0f931bc318a51c078776e755776fc8c10f51830
rzeraat.tur <rzeraat.tur@gmail.com>
2024-01-22 21:39:55 +0300

    Add form validation and registration functionality to the register component

--- front-end-mvp/src/app/features/auth/auth.module.ts
+++ front-end-mvp/src/app/features/auth/auth.module.ts
@@ -5,19 +5,15 @@ import { RegisterComponent } from './register/register.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { ProfileComponent } from './profile/profile.component';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     LoginComponent,
     RegisterComponent,
     ForgotPasswordComponent,
-    ProfileComponent
+    ProfileComponent,
   ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- front-end-mvp/src/app/features/auth/register/register.component.ts
+++ front-end-mvp/src/app/features/auth/register/register.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsersService } from 'src/app/core/use-cases/users.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private usersService: UsersService // Inject the UsersService
+  ) {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.usersService.registerUser(this.registerForm.value).subscribe(
+        (user) => {
+          console.log('Registration successful', user);
+          // Handle registration success
+        },
+        (error) => {
+          console.error('Registration failed', error);
+          // Handle registration error
+        }
+      );
+    }
+  }
 }
