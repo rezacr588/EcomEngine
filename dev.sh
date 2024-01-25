630c3a3d70971e9663222686f393a86d3bd4e0eb
rzeraat.tur <rzeraat.tur@gmail.com>
2024-01-26 01:56:53 +0300

    Add new files and update existing files

--- dev.sh
+++ dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the front-end directory and start the front-end server in the background
-cd front-end-mvp
+cd front-end
 yarn start &
 
 # Navigate to the API directory and start the API server
